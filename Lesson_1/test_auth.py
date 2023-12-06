4385e661e127ffd36ae775ecf9ed35a76e76df86
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2023-12-06 17:58:57 +0400

    Feast: Add Lesson_1 directory

--- Lesson_1/test_auth.py
+++ Lesson_1/test_auth.py
@@ -1,8 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pytest
 
 driver = webdriver.Chrome()
+
+
+# @pytest.fixture(scope='function')
+# def driver():
+#     driver = webdriver.Chrome()  # Initialize WebDriver (Chrome example)
+#     yield driver
+#     driver.quit()
+
+
+@pytest.fixture(scope='function')
+def log_in():
+    driver.get('https://www.saucedemo.com/')
+    user_name_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    user_name_field.send_keys('standard_user')
+    time.sleep(1)
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys('secret_sauce')
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+    yield driver
+    driver.quit()
 
 
 def test_login_form():
@@ -13,12 +36,12 @@ def test_login_form():
 
     time.sleep(1)
 
-    password_field = driver.find_element(By.XPATH,'//input[@data-test="password"]')
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
     password_field.send_keys('secret_sauce')
 
     time.sleep(1)
 
-    login_button = driver.find_element(By.XPATH,'//input[@data-test="login-button"]')
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
     login_button.click()
 
     time.sleep(5)
