4fc606b30dcb5fb10f84f9d66d1d1073b54cc109
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2023-12-10 20:43:42 +0400

    Feat: Add test_product_card.py.py

--- Lesson_1/test_logout.py
+++ Lesson_1/test_logout.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-import time
 import pytest
 from test_auth import log_in
 
