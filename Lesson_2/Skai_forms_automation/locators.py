46b247b9730303b3e29eb90b91a632398888f590
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2023-12-17 16:14:13 +0400

    Delete: Skai automation

--- Lesson_2/Skai_forms_automation/locators.py
+++
@@ -1,7 +0,0 @@
-# PARDOT
-pardot_login_button_primary = "//a[@id = 'logInWithSalesforceButton']"
-pardot_username_field = '//*[@id="username"]'
-pardot_password_field = '//*[@id="password"]'
-pardot_login_button = '//*[@id="Login"]'
-verification_code_field = '//*[@id="tc"]'
-verification_button = '//*[@id="save"]'
